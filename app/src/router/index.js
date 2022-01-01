5d4dcedafafc5b40a4be00e29f3900bddbd04bf3
dev_zds <229026709@qq.com>
2022-01-01 13:36:01 +0800

    2022.1.1

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -30,10 +30,9 @@ const routes = [
     }]
   }
 ];
-
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  base: process.env.NODE_ENV === 'production' ? '/app/' : process.env.BASE_URL,
   routes,
 });
 
