ea0da302ed5e14dd183dbb431e13efdadc17b533
dev_zds <229026709@qq.com>
2022-01-04 15:26:47 +0800

    前端代码打包

--- app/src/views/home/index.vue
+++ app/src/views/home/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>home</div>
+  <div>
+    <div @click="go('dev_zds')">dev_zds</div>
+  </div>
 </template>
 
 <script>
 export default {
+  mounted() {
 
+  },
+  methods: {
+    go(url) {
+      this.$router.push('/blogger/' + url)
+    }
+  }
 }
 </script>
 
