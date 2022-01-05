ce452a8bde849925e86ad28d08c63d9a752c39fc
dev_zds <229026709@qq.com>
2022-01-05 09:03:06 +0800

    前端代码打包

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -32,7 +32,7 @@ const routes = [
 ];
 const router = new VueRouter({
   mode: "history",
-  base: process.env.NODE_ENV === 'production' ? '/app' : process.env.BASE_URL,
+  base: 'app',
   routes,
 });
 
