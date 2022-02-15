e2432e96efa8b795854f65572673b1b3198da12a
dev_zds <229026709@qq.com>
2022-02-15 16:29:23 +0800

    修改websocket地址

--- app/src/main.js
+++ app/src/main.js
@@ -23,8 +23,8 @@ Vue.config.productionTip = false;
 Vue.use(VueAxios)
 Vue.use(new VueSocketIO({
   // debug: true,
-  // connection: 'http://127.0.0.1:7001', //服务端连接
-  connection: 'http://127.0.0.1:8000', //服务端连接
+  connection: 'http://127.0.0.1:80', //服务端连接
+  // connection: 'http://124.222.47.163:7001', //服务端连接
   reconnection: true,
   reconnectionAttempts: Infinity,
   options: {
