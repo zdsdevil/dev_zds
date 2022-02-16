8df24a52a8b6ba3cb07a2b3323a4ac9c54ea4335
dev_zds <229026709@qq.com>
2022-02-16 23:05:50 +0800

    前端代码打包

--- app/src/config/fileConfig.js
+++ app/src/config/fileConfig.js
@@ -1,1 +1,1 @@
-export const fileBaseUrl = process.env.NODE_ENV === 'production' ? 'http://121.40.119.210:7001/public' : 'http://127.0.0.1:7001/public'
+export const fileBaseUrl = process.env.NODE_ENV === 'production' ? 'http://124.222.47.163:7001/public' : 'http://127.0.0.1:7001/public'
